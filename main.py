a3bc7231ec8e7feebfa65ea97760a199f9fd1f8d
Alobin90 <89987762+Alobin90@users.noreply.github.com>
2023-10-20 16:17:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+def main():
 intents = discord.Intents().all()
 client = commands.Bot(command_prefix='u!', intents=intents)
 client.remove_command('info')
@@ -14,3 +15,6 @@ async def tank(ctx):
     await ctx.send("https://i.pinimg.com/originals/88/80/72/8880722f932c12f4a93ce18c389af130.jpg")
 
 client.run("MTAxNzg5MTE0ODUzNjIzODE0MA.G6RrRl.YoD1N5kj9e5YcHYgJoB546IBlV4W0WfwTls1NY")
+
+if __main__ = '__main__':
+    main()
